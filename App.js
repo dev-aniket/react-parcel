23bf36b30c977397af986881e4beb5064892c1e6
Aniket Mallick <aniketmallick@192.168.1.4>
2026-04-19 11:11:36 +0530

    Functional Components

--- App.js
+++ App.js
@@ -1,4 +1,21 @@
-const heading = React.createElement("h1", {id: "heading"}, "Hellow from React");
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+
+const title =  (
+    <h1 id="heading">React using JSX</h1>
+);
+
+
+
+const HeadingComponent = () => (
+    <div className="container">
+        <h2>{200 + 250}</h2>
+        {title}
+        <h1 id="heading">React from functional component</h1>
+    </div>
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
+
